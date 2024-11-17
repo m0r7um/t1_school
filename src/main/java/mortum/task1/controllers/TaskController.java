6d88ccb2d4e8c4408f8883e921cea626f1c47ab6
martin <martin.valtz@gmail.com>
2024-11-18 00:41:19 +0300

    add: aspect logging

--- src/main/java/mortum/task1/controllers/TaskController.java
+++ src/main/java/mortum/task1/controllers/TaskController.java
@@ -1,6 +1,8 @@
 package mortum.task1.controllers;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import mortum.task1.aspects.annotations.*;
 import mortum.task1.persistence.models.Task;
 import mortum.task1.services.TaskService;
 import org.springframework.http.ResponseEntity;
@@ -8,32 +10,41 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/tasks")
 @RequiredArgsConstructor
 public class TaskController {
     final TaskService taskService;
 
+    @GetMethodLogging
     @GetMapping
     public List<Task> getTask() {
         return taskService.getAllTasks();
     }
 
+    @GetMethodLogging
     @GetMapping("/{id}")
     public ResponseEntity<Task> getTaskById(@PathVariable Integer id) {
         return ResponseEntity.ok(taskService.getTask(id));
     }
 
+    @ExceptionLogging
+    @IncomingPostRequestLogging
     @PostMapping
     public Task addTask(@RequestBody Task task) {
         return taskService.createTask(task);
     }
 
+    @ExceptionLogging
+    @ModifyingOperationLogging
+    @IncomingPutRequestLogging
     @PutMapping("/{id}")
     public Integer updateTask(@RequestBody Task task, @PathVariable Integer id) {
         return taskService.updateTask(task, id);
     }
 
+    @ModifyingOperationLogging
     @DeleteMapping("/{id}")
     public Integer deleteTask(@PathVariable Integer id) {
         return taskService.deleteTask(id);
